73bee2aea09f64308514f3eb9051af3be5ac0da9
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-03-07 23:03:56 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -4,10 +4,10 @@ public class Node {
    
     // a pointer to a CharData object
     CharData cp;
-
+ 
     // a pointer to a Node object
     Node next;  // pointer
-   
+ 
     /** Constructs a node with the given CharData object.
      *  The new node will point to the given next node. */
     public Node(CharData cp, Node next) {
